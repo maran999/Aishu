f254ff863f516cd63324fdee26ea7dc229827be5
Maran <manimaran.gopinathan@gmail.com>
2024-11-11 23:05:28 +0530

    Manimaran commit

--- src/main/java/socialMedia/fb/App.java
+++ src/main/java/socialMedia/fb/App.java
@@ -3,5 +3,8 @@ package socialMedia.fb;
 public class App {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    System.out.println("Hello World!");
+    System.out.println("Hello World!");
+    System.out.println("Hello World!");
   }
 }
